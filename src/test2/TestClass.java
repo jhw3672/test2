dbc7d971650914ab7ec263fa0a263865155ceb6c
WonJae Choi <choiwj1012@hanmail.net>
2017-11-18 16:45:42 +0900

    [test-001] 로그인 기능 구현
    id값과 password값이 일치할 경우 로그인이 성공하셨습니다 라는 메세지 표현

---
+++ src/test2/TestClass.java
@@ -0,0 +1,21 @@
+package test2;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+
+		String id = "testid";
+		
+		String passwd = "1234";
+		
+		if(id=="testid") {
+			
+			if(passwd=="1234") {
+				
+				System.out.println("로그인이 성공하셨습니다");
+				
+			}
+		}	
+	}
+	
+}
